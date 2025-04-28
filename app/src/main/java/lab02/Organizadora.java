08889c5c144c6edcb9a6ba6bc1d4a1e9d4f4b9b8
Jose Paliares <josearthurpaliares@gmail.com>
2025-04-27 21:01:18 -0300

    Create criarEvento method with overloads for different event types (EventoFestival, EventoJogo, EventoShow

--- app/src/main/java/lab02/Organizadora.java
+++ app/src/main/java/lab02/Organizadora.java
@@ -1,5 +1,7 @@
 package lab02;
 
+import java.util.List;
+
 public class Organizadora {
 
     private String nome;
@@ -19,5 +21,41 @@ public class Organizadora {
     }
 
     
-    
+    /**
+     * Cria um evento do tipo EventoFestival
+     * @param nome o nome do evento
+     * @param local o local do evento
+     * @param precoIngresso o preço do ingresso do evento
+     * @param data a data do evento
+     * @param lineup a lista de artistas que se apresentarão no evento
+     * @param duracao a duração do evento em horas
+     * @return um evento do tipo EventoFestival
+     */
+    public EventoFestival criarEvento(String nome, Local local, double precoIngresso, String data, List<String> lineup, int duracao) {
+        return new EventoFestival(nome, local, precoIngresso, this, data, lineup, duracao);
+    }
+    /**
+     * Cria um evento do tipo EventoJogo
+     * @param nome o nome do evento
+     * @param local o local do evento
+     * @param precoIngresso o preço do ingresso do evento
+     * @param data a data do evento
+     * @param times a lista de times que se enfrentarão no evento
+     * @return um evento do tipo EventoJogo
+     */
+    public EventoJogo criarEvento(String nome, Local local, double precoIngresso, String data, List<String> times) {
+        return new EventoJogo(nome, local, precoIngresso, this, data, times);
+    }
+    /**
+     * Cria um evento do tipo EventoShow
+     * @param nome o nome do evento
+     * @param local o local do evento
+     * @param precoIngresso o preço do ingresso do evento
+     * @param data a data do evento
+     * @param artista o artista que se apresentará no evento
+     * @return um evento do tipo EventoShow
+     */
+    public EventoShow criarEvento(String nome, Local local, double precoIngresso, String data, String artista) {
+        return new EventoShow(nome, local, precoIngresso, this, data, artista);
+    }
 }
